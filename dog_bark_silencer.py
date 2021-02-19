d8b8e39235a2c3c75d9fa986a9e6e3c9b44cdb58
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2021-02-19 18:58:00 +0100

    Added python script

---
+++ dog_bark_silencer.py
@@ -0,0 +1,30 @@
+from playsound import playsound
+import RPi.GPIO as GPIO
+import time
+
+sounds = [
+    "sound1.mp3",
+    "sound2.mp3",
+    "sound3.mp3",
+    "sound4.mp3",
+    "sound5.mp3"
+]
+
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        on_bark_detected()
+
+def on_bark_detected():
+    global sounds
+    random_sound = random.choice(sounds)
+    playsound(random_sound)
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    time.sleep(1)
